157b0ad3cacaf7e745ede9f0fe66a241d180cafc
Maria <91953276+mariasnchez@users.noreply.github.com>
2023-02-23 23:26:46 +0100

    Add files via upload

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,30 +1,30 @@
 #! /bin/bash
-STACK_NAME=default
+STACK_NAME=cloudDeploy
 REGION=us-east-1
 CLI_PROFILE=default
 
 EC2_INSTANCE_TYPE=t2.micro
 
-echo -e "\n=========== Desplegando  setup.yml ================="
-
-aws cloudformation deploy \
-	--region $REGION \
-	--profile $CLI_PROFILE \
-	--stack-name $STACK_NAME-setup \
-	--template-file setup.yml \
-	--no-fail-on-empty-changeset \
-	--capabilities CAPABILITY_NAMED_IAM \
-
-#echo -e "\n=========== Desplegando  main.yml ================="
+#echo -e "\n=========== Desplegando  setup.yml ================="
 
 #aws cloudformation deploy \
 #	--region $REGION \
 #	--profile $CLI_PROFILE \
-#	--stack-name $STACK_NAME \
-#	--template-file main.yml \
+#	--stack-name $STACK_NAME-setup \
+#	--template-file setup.yml \
 #	--no-fail-on-empty-changeset \
 #	--capabilities CAPABILITY_NAMED_IAM \
-#	--parameter-override EC2InstanceType=$EC2_INSTANCE_TYPE 
+
+echo -e "\n=========== Desplegando  main.yml ================="
+
+aws cloudformation deploy \
+	--region $REGION \
+	--profile $CLI_PROFILE \
+	--stack-name $STACK_NAME \
+	--template-file main.yml \
+	--no-fail-on-empty-changeset \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--parameter-override EC2InstanceType=$EC2_INSTANCE_TYPE 
 
 	if [$? -eq 0]; then
 		aws cloudformation list-exports \
